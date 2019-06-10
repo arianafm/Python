a93a950175ee762599a8ad5c87427526c3da5b3e
arianafm <arianafm@ciencias.unam.mx>
2019-06-10 10:39:59 -0500

    Primera clase.

---
+++ Clase1/holamundo.py
@@ -0,0 +1,96 @@
+# Hola mundo.
+print("Hola mundo.")
+
+
+# O P E R A C I O N E S:
+
+# Operación con enteros:
+print(5+7)
+
+# Operación con complejos
+print((4+1j)+(2+3j))
+
+# Operación división
+print(7/2)
+
+# Operación división entera
+print(7//2)
+
+# Operación con módulo
+print(10%8)
+
+# Operación con potencia:
+print(5**2)
+
+#Comentario de una línea.
+
+"""
+Comentario multilinea.
+
+Tipos de datos:
+-Enteros
+-Flotantes
+-Complejos
+
+Operadores:
++ -- Suma
+- -- Resta
+* -- Multiplicación
+/ -- División
+** -- Potencia
+// -- División entera
+% -- Módulo
+
+"""
+
+# V A R I A B L E S:
+
+x = 5
+y = 2.5
+z = x+y
+print(z)
+
+# Imprime el tipo de dato al cual pertenece z.
+# En este caso es un float.
+print(type(z))
+
+"""
+Booleanos:
+-True 
+-False
+
+Operadores relaciones:
+> -- Mayor que
+>= -- Mayor ó igual que
+< -- Menor que
+<= -- Menor ó igual que
+== -- Equivalente
+!= -- Diferente de 
+
+"""
+# B O O L E A N O S:
+
+#Operador booleano "Mayor que":
+print(x>y)
+
+#Operador booleano "Diferente de":
+print(x!=y)
+
+a = True
+b = False
+
+# and, or, not
+
+print(a and b)
+print(a or b)
+print(not a)
+
+print((a and b) and (a or b))
+print((a and b) or (a or b))
+
+# Imprime el tipo de dato al cual pertenece a.
+# En este caso es un bool.
+
+print(type(a))
+
+
